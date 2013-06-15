da663736555f1e14cb082bdc63829bfddb6a54ec
rafaellucio <rafaelantoniolucio@gmail.com>
2013-06-15 14:44:58 -0300

    add lista

--- js/default.js
+++ js/default.js
@@ -1,3 +1,27 @@
 var $  = function (elem) {
 	return document.querySelectorAll(elem);
 }
+
+
+var lista = [
+	{
+		todo: 'adsfksjldfhlkjadsfhkladsjf',
+		done: true
+	},
+	{
+		todo: 'outro',
+		done: false
+	}
+]
+
+window.load = function () {
+
+	var lis = [];
+	lista.forEach(function(item){
+		var li = "<li>" + item.todo + "</li>";
+		lis.push(li);
+	});
+
+	$('.list-item ul')[0].append(lis);	
+}
+
